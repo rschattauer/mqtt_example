0658920b69d61b8228c89123bdda57be03814246
Richard Schattauer <richard.schattauer@jakala.com>
2025-11-17 19:03:51 +0100

    Add example kmp/cmp project

---
+++ composeApp/src/commonTest/kotlin/com/example/mqttexampletest/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.mqttexampletest
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
